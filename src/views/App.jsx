48a40bceb4259db77433842c26d7969a6835a19a
oricis <m_alc1@hotmail.com>
2019-10-20 11:59:53 +0200

    tested API calls

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,21 +1,84 @@
 import '../assets/styles/App.scss';
-import React from 'react';
-import FilmList from "./film-list/FilmList";
-import Footer from "./footer/Footer";
-import Header from "./header/Header";
+import React,  { Component } from 'react';
+import FilmList from './film-list/FilmList';
+import Footer from './footer/Footer';
+import Header from './header/Header';
+import AxiosApi from '../services/AxiosApi.service.js';
 
-function App()
+class App extends Component
 {
-    const data = [];
-    const pageSubtitle = '';
+    constructor()
+    {
+        super();
 
-    return (
-        <div className="App">
-            <Header pageSubtitle={pageSubtitle}></Header>
-            <FilmList data={data}></FilmList>
-            <Footer></Footer>
-        </div>
-    );
+        this.categories = ['top_rated', 'upcoming', 'popular'];
+        this.data = [];
+        this.pageSubtitle = '';
+
+        this.state = {
+            category: '',
+            fetched:  0,
+            films:    [],
+            error:    '',
+        };
+    }
+
+    componentDidMount() // before render()
+    {
+        this.getFilms(this.categories[0]);
+    }
+
+    componentDidUpdate() // after render()
+    {
+        console.log('componentDidUpdate() - FILMS: ', this.state.films);
+    }
+
+    render()
+    {
+        const pageSubtitle = (this.state.category)
+            ? this.state.category + ' Films'
+            : this.pageSubtitle;
+        console.log('@@@@@@@@', this.state.films);
+
+        return (
+            <div className='App'>
+                <Header pageSubtitle={pageSubtitle}></Header>
+                <FilmList films={this.state.films}></FilmList>
+                <Footer></Footer>
+            </div>
+        );
+    }
+
+
+    /**
+     * Custom methods
+     *
+     */
+
+    async getFilms(category)
+    {
+        category = (category) ? category : this.categories[0];
+
+        // if (category !== this.state.category && !this.state.loading) {
+        if (this.state.fetched <= 2) {
+
+            try {
+                const data = await AxiosApi.getMoviesByCategory(category);
+                this.setState({
+                    category: category,
+                    fetched: 2,
+                    films: data.results,
+                    error: '',
+                });
+            } catch {
+                this.setState({
+                    category: category,
+                    fetched: 2,
+                    error: 'Fail fetching movies',
+                });
+            }
+        } /**/
+    }
 }
 
 export default App;
--- src/views/film-list/FilmList.jsx
+++ src/views/film-list/FilmList.jsx
@@ -4,35 +4,21 @@ import PropTypes from 'prop-types';
 
 class FilmList extends Component
 {
-    constructor(props)
-    {
-        super(props);
-
-        // Set default values
-        this.state = {
-            films: [],
-        };
-    }
 
     render()
     {
 
         return (
             <section className="App-filmlist">
-                // Filmm searcher
+                <p>Retrieved Films: {this.props.films.length}</p>
 
-                {
-                    (this.state.films.length > 0)
-                        ? '<Card></Card>'
-                        : ''
-                }
             </section>
         );
     }
 
     componentDidUpdate()
     {
-        console.log('componentDidUpdate() - Fimls: ' + this.state.films.length); // HACK:
+        console.log('FilmList / componentDidUpdate() - Fimls: ' + this.props.films.length); // HACK:
     }
 
 
