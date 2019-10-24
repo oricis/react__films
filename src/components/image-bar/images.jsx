add09c40a5ebf5ea61effd543b4af8ecd1b3afe9
oricis <m_alc1@hotmail.com>
2019-10-24 22:49:23 +0200

    refactorized component: "Image-Bar"

---
+++ src/components/image-bar/images.jsx
@@ -0,0 +1,5 @@
+export const IMG1 = require('../../assets/images/2IUpoKSP64r6rp2vBo0Fdk8a1UU.jpg');
+export const IMG2 = require('../../assets/images/c3F4P2oauA7IQmy4hM0OmRt2W7d.jpg');
+export const IMG3 = require('../../assets/images/skvI4rYFrKXS73BJxWGH54Omlvv.jpg');
+export const IMG4 = require('../../assets/images/uLXK1LQM28XovWHPao3ViTeggXA.jpg');
+export const IMG5 = require('../../assets/images/xOcuDnxTTqNT2CuFNy5VVc67tG4.jpg');
