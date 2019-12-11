2a331343709f0878ed6c1ba7bfb14a62817bc705
moises.alcocer <contacto@ironwoods.es>
2019-12-11 08:50:53 +0100

    added files to deploy

---
+++ docs/precache-manifest.f055b76b300c0826f78f262e7e8a1b64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512b1903aaf390c318b61e314a878d2d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43b76ad9b7adeb024e79",
+    "url": "./static/css/main.7bce8777.chunk.css"
+  },
+  {
+    "revision": "ac9bcf99201121ec4e61",
+    "url": "./static/js/2.4a81aeae.chunk.js"
+  },
+  {
+    "revision": "43b76ad9b7adeb024e79",
+    "url": "./static/js/main.14a4f3e9.chunk.js"
+  },
+  {
+    "revision": "f95ffb848c44423ff641",
+    "url": "./static/js/runtime-main.b46a0611.js"
+  },
+  {
+    "revision": "f4062e4c5192e21ac7b2c8b1e0506b15",
+    "url": "./static/media/2IUpoKSP64r6rp2vBo0Fdk8a1UU.f4062e4c.jpg"
+  },
+  {
+    "revision": "0afab2969070efe6f17d0dba9f47dcb9",
+    "url": "./static/media/xOcuDnxTTqNT2CuFNy5VVc67tG4.0afab296.jpg"
+  }
+]);
